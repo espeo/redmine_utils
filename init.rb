745ed44bbb7d866c96653d74ddbe79290a44d19e
Jacek Tomaszewski <jacek@jtom.me>
2015-06-30 15:46:45 +0200

    1.0.1

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :espeo_utils do
   name 'Utils plugin'
   author 'espeo@jtom.me'
   description "Utillity classes and helpers used by Espeo redmine plugins."
-  version '1.0.0'
+  version '1.0.1'
   url 'http://espeo.pl'
   author_url 'http://jtom.me'
 end
